9f5e5fda159d708d06e5183698230d2437e56ca6
Umut Şar <contact@umutsar.net>
2024-05-27 21:59:55 +0300

    Create 4-finger-listener.sh

---
+++ 4-finger-listener.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# listen touchpad with "libinput debug-events" tools  
+libinput debug-events | grep --line-buffered 'GESTURE_HOLD' | while read line
+do
+    if echo "$line" | grep -q 'GESTURE_HOLD_BEGIN.*4'; then
+        xdotool keydown Super key v keyup Super # Opening notification pop-up. You can change according to your request
+    elif echo "$line" | grep -q 'GESTURE_HOLD_END.*4'; then
+        xdotool key Escape # Pop-up opens when 4 fingers holding on the touchpad, if you remove your fingers "Escape" key working, then pop-up closes.
+    fi
+done
+
+# download this file and execute this commands:
+# 1- sudo chmod +x 4-finger-listener.sh
+# 2- ./4-finger-listener.sh
+
+# If you want to execute this script everytime, you can add this script in the startup application. 
+# I have written startup application-scripts subject in the README.md file.
